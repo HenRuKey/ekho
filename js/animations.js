a0f60a0827418b15176402ceb1235a32cf567e7b
Keaton Henderson <keaton@sdsix.com>
2018-05-26 13:48:16 -0600

    added new sections

--- js/animations.js
+++ js/animations.js
@@ -5,11 +5,29 @@
         // Create instance of scroll reveal
         window.sr = ScrollReveal({reset: true});
         
-        //  Navigation
+        // Navigation
         sr.reveal('.navbar', {
             duration: 1000,
             origin: 'top'
         });
+
+        // Product image
+        sr.reveal('.product-image', {
+            duration: 1000,
+            origin: 'bottom'
+        });
+
+        // Product description
+        sr.reveal('.product-description', {
+            duraiton: 1000,
+            origin: 'bottom'
+        });
+
+        sr.reveal('.in-between-content', {
+            duration: 1000,
+            origin: 'bottom'
+        });
+
     });
     
 })();
