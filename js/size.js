070a971df3db75764179eff93518c921f8b00185
Keaton Henderson <keaton@sdsix.com>
2018-05-25 18:06:55 -0600

    background image and possible future video functionality

---
+++ js/size.js
@@ -0,0 +1,22 @@
+(function() {
+    $(window).ready(function() {
+        resizeMainBlock();
+    });
+
+    $(window).resize(function() {
+        resizeMainBlock();
+    });
+
+    /**
+     * Adjusts the size of the main-block div to fill the remaining
+     *      space below the nav.
+     */
+    function resizeMainBlock() {
+        var navHeight = $("nav").height();
+        var navPadding = $(".navbar").css("padding-bottom").replace('px', '');
+        var viewportHeight = $(window).height();
+        var mainBlockHeight = viewportHeight - navHeight - (navPadding * 2);
+        $(".main-block").css('height', mainBlockHeight);
+    }
+
+})();
